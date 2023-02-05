6be6a16eb3cefc2450b56d157df4ee054ae1cc65
Gaurav Kumar <66291225+gaurav645@users.noreply.github.com>
2023-02-05 10:46:56 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import ChatDashboard from "./ChatDashboard";
 class App extends Component {
     state = {
         messages: [
-            {username: 'Asad', text: 'Hi, Ali!'},
-            {username: 'Asad', text: 'How are you?'},
-            {username: 'Ali', text: 'Hi, Asad! Good, you?'},
+            {username: 'Gaurav', text: 'Hi, Daisy!'},
+            {username: 'Gaurav', text: 'How are you?'},
+            {username: 'Daisy', text: 'Hi, Gaurav! Good, you?'},
         ],
-        users: [{username: 'Asad'}, {username: 'Ali'}]
+        users: [{username: 'Gaurav'}, {username: 'Daisy'}]
     };
 
     updateMessageListing = (message) => {
@@ -28,7 +28,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">ReactND - Coding Practice</h1>
+                    <h1 className="App-title">React Messenger</h1>
                 </header>
                 <div className="container">
                     {
